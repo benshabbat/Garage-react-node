8c4076591a43d38f9be390c09bd8986a4b9c78a0
benshabbat <benshabbat27@gmail.com>
2023-12-25 20:40:37 +0200

    fix ui table and added filter

--- client/src/pages/account/Account.jsx
+++ client/src/pages/account/Account.jsx
@@ -3,12 +3,13 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import ReqService from "../../components/create/ReqService";
 import useOpenModel from "../../hooks/useOpenModel";
-import {useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 const Account = () => {
-  const {user } = useSelector((state) => state.user);
+  const { user } = useSelector((state) => state.user);
   const [car, setCar] = useState();
   const [handleReqService, isOpenReqService] = useOpenModel();
-
+  const [filterCars, setFilterCars] = useState();
+  const navigate = useNavigate();
   const handelCar = (e) => {
     const { value } = e.target;
     console.log(e.target.value);
@@ -16,51 +17,75 @@ const Account = () => {
     handleReqService();
   };
 
-  const navigate = useNavigate();
 
   const onServices = (e) => {
     const { value } = e.target;
     console.log(e.target.value);
     navigate(`/services/car/${value}`);
   };
+
+  const filterSearch = (e) => {
+    const { value } = e.target;
+    setFilterCars(
+      user?.cars?.filter(
+        (item) =>
+          item.username.includes(value) ||
+          item.numberPlate.includes(value) ||
+          item.km.toString().includes(value) ||
+          item.brand.includes(value)
+      )
+    );
+  };
   return (
     <>
-      <section className="table__body">
-        <table>
-          <thead>
-            <tr>
-              <th>brand</th>
-              <th>numberPlate</th>
-              <th>km</th>
-              <th>history service</th>
-              <th>Request Service</th>
-            </tr>
-          </thead>
-          <tbody>
-            {user?.cars?.[0]?._id &&
-              user?.cars?.map((car) => {
-                return (
-                  <tr key={car._id}>
-                    <td>{car.brand}</td>
-                    <td>{car.numberPlate}</td>
-                    <td>{car.km}</td>
-                    <td>
-                      <button value={car._id} onClick={onServices}>
-                        services
-                      </button>
-                    </td>
-                    <td>
-                      <button value={car._id} onClick={handelCar}>
-                        req services
-                      </button>
-                    </td>
-                  </tr>
-                );
-              })}
-          </tbody>
-        </table>
-      </section>
-
+      {" "}
+      <div className="table-container">
+        <section className="table__header">
+          <h1>My Cars</h1>
+          <div className="input-group">
+            <input
+              type="search"
+              placeholder="Search Data..."
+              onChange={filterSearch}
+            />
+          </div>
+        </section>
+        <section className="table__body">
+          <table>
+            <thead>
+              <tr>
+                <th>brand</th>
+                <th>numberPlate</th>
+                <th>km</th>
+                <th>history service</th>
+                <th>Request Service</th>
+              </tr>
+            </thead>
+            <tbody>
+              {user?.cars?.[0]?._id &&
+                user?.cars?.map((car) => {
+                  return (
+                    <tr key={car._id}>
+                      <td>{car.brand}</td>
+                      <td>{car.numberPlate}</td>
+                      <td>{car.km}</td>
+                      <td>
+                        <button value={car._id} onClick={onServices}>
+                          services
+                        </button>
+                      </td>
+                      <td>
+                        <button value={car._id} onClick={handelCar}>
+                          req services
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+        </section>
+      </div>
       {
         <ReqService
           car={car}
