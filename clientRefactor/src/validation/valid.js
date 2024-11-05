ce44a1ee779f2d949042bc5eb4a873388ba59173
benshabbat <benshabbat27@gmail.com>
2024-11-05 09:47:56 +0200

    moved every function into file

--- clientRefactor/src/validation/valid.js
+++ clientRefactor/src/validation/valid.js
@@ -1,27 +1,10 @@
 import validUser from "./validUser.js";
 import validPhone from "./validPhone.js";
+import validCar from "./validCar.js";
+import validEmail from "./validEmail.js";
+import validPass from "./validPass.js";
 
-const CAR_REGEX =
-  /^[0-9]{3}[-][0-9]{2}[-][0-9]{3}|[0-9]{2}[-][0-9]{3}[-][0-9]{2}|[0-9]{7,8}$/;
-const EMAIL_REGEX =
-  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-const PASS_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
 
-const validCar = (data) => {
-  if (
-    ((data?.length === 8 || data?.length === 7) && +data) ||
-    (data?.length === 10 && data.at(3) === "-" && data.at(6) === "-") ||
-    (data?.length === 9 && data.at(2) === "-" && data.at(6) === "-")
-  ) {
-    return CAR_REGEX.test(data);
-  } else return false;
-};
-const validPass = (data) => {
-  return PASS_REGEX.test(data);
-};
-const validEmail = (data) => {
-  return EMAIL_REGEX.test(data);
-};
 
 const valid = (data, type) => {
   switch (type) {
