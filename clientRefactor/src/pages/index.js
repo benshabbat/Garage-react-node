da382c0fd52f82b194a6e86159a134b8f6fc3392
benshabbat <benshabbat27@gmail.com>
2024-11-05 10:03:12 +0200

    move indexes fro comps and pages and the comp header

---
+++ clientRefactor/src/pages/index.js
@@ -0,0 +1,18 @@
+import { lazy } from "react";
+export { default as Account } from "./account/Account";
+export { default as Home } from "./home/Home";
+export { default as PageLanding } from "./pageLanding/PageLanding";
+export { default as ServicesAdmin } from "./ServicesAdmin";
+
+//Want to check what is the best practice for lazy loading
+
+// export { default as Messages } from "./messages/Messages";
+// export { default as Services } from "./services/Services";
+// export { default as Users } from "./Users";
+// export { default as Cars } from "./Cars";
+
+//Want to check what is the best practice for lazy loading
+export const Users = lazy(()=>import("./Users"))
+export const Messages = lazy(()=>import("./messages/Messages"))
+export const Cars = lazy(()=>import("./Cars"))
+export const Services = lazy(()=>import("./services/Services"))
