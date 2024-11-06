e3d7f7dcb1ead7767f7f70b45476f6aab6aa9dbb
benshabbat <benshabbat27@gmail.com>
2024-11-06 22:31:49 +0200

    clean code

--- client/src/components/landing/reviews/Reviews.jsx
+++ client/src/components/landing/reviews/Reviews.jsx
@@ -19,25 +19,7 @@ const Reviews = () => {
   const handelClick = () => {
     handleAddReview();
   };
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 1024 },
-      items: 4,
-    },
-    desktop: {
-      breakpoint: { max: 1024, min: 800 },
-      items: 3,
-    },
-    tablet: {
-      breakpoint: { max: 800, min: 564 },
-      items: 2,
-    },
-    mobile: {
-      breakpoint: { max: 564, min: 0 },
-      items: 1,
-    },
-  };
+  
   return (
     <div id="reviews">
       <h1>Reviews</h1>
@@ -47,11 +29,6 @@ const Reviews = () => {
             return <Review customer={customer} key={index} />;
           })}
         </CardSwiper>
-        {/* <Carousel infinite={true} responsive={responsive}>
-        {allReviews?.map((customer, index) => {
-            return <Review customer={customer} key={index} />;
-          })}
-        </Carousel> */}
       </div>
       <button className="btn-review" onClick={handelClick}>
         Add Review
