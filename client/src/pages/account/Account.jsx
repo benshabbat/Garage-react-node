92318741f6f4e65567c4c241db6007e63e7a5bda
benshabbat <benshabbat27@gmail.com>
2024-11-07 16:02:54 +0200

    moved all logic of page to another file

--- client/src/pages/account/Account.jsx
+++ client/src/pages/account/Account.jsx
@@ -1,63 +1,20 @@
 import "./account.css";
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import ReqService from "../../components/create/ReqService";
-import useOpenModel from "../../hooks/useOpenModel";
-import { useSelector } from "react-redux";
+import { useAccount, bodyAcc } from "./utilsAccount";
 const Account = () => {
-  const { user } = useSelector((state) => state.user);
-  const [car, setCar] = useState();
-  const [handleReqService, isOpenReqService] = useOpenModel();
-  const [filterCars, setFilterCars] = useState();
-  const navigate = useNavigate();
-  const handelCar = (e) => {
-    const { value } = e.target;
-    console.log(e.target.value);
-    setCar(user?.cars.find((c) => c._id === value));
-    handleReqService();
-  };
+  const {
+    filterSearch,
+    onServices,
+    handelCar,
+    car,
+    isOpenReqService,
+    filterCars,
+    handleReqService,
+    user,
+  } = useAccount();
 
-
-  const onServices = (e) => {
-    const { value } = e.target;
-    console.log(e.target.value);
-    navigate(`/services/car/${value}`);
-  };
-
-  const filterSearch = (e) => {
-    const { value } = e.target;
-    setFilterCars(
-      user?.cars?.filter(
-        (item) =>
-          item.numberPlate.includes(value) ||
-          item.km.toString().includes(value) ||
-          item.brand.includes(value)
-      )
-    );
-  };
-  const bodyAcc =(car)=>{
-    return(
-      <tr key={car._id}>
-      <td>{car.brand}</td>
-      <td>{car.numberPlate}</td>
-      <td>{car.km}</td>
-      <td>
-        <button value={car._id} onClick={onServices}>
-          services
-        </button>
-      </td>
-      <td>
-        <button value={car._id} onClick={handelCar}>
-          req services
-        </button>
-      </td>
-    </tr>
-    )
-
-  }
   return (
     <>
-      {" "}
       <div className="table-container">
         <section className="table__header">
           <h1>My Cars</h1>
@@ -65,7 +22,7 @@ const Account = () => {
             <input
               type="search"
               placeholder="Search Data..."
-              onChange={filterSearch}
+              onChange={(e) => filterSearch(e)}
             />
           </div>
         </section>
@@ -81,7 +38,9 @@ const Account = () => {
               </tr>
             </thead>
             <tbody>
-              {filterCars ? filterCars?.map(bodyAcc) :user?.cars?.map((bodyAcc))}
+              {filterCars
+                ? filterCars?.map((car) => bodyAcc(car, onServices, handelCar))
+                : user?.cars?.map((car) => bodyAcc(car, onServices, handelCar))}
             </tbody>
           </table>
         </section>
--- clientRefactor/src/pages/account/Account.jsx
+++ clientRefactor/src/pages/account/Account.jsx
@@ -1,5 +1,5 @@
 import "./account.css";
-import React, { useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ReqService from "../../components/create/ReqService";
 import useOpenModel from "../../hooks/useOpenModel";
