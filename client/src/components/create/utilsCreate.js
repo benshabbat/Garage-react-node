2e0a388a10451e95f76ea0dad0cd178ebe2ae1c6
benshabbat <benshabbat27@gmail.com>
2024-11-10 12:35:11 +0200

    cleanCode

--- client/src/components/create/utilsCreate.js
+++ client/src/components/create/utilsCreate.js
@@ -1,47 +1,12 @@
-import { useState } from "react";
 import { validPhone, validPass, validEmail } from "../../validation/valid";
-import { createUser } from "../../utils";
-// export const useOnSubmitRegister = async ( formData, handelClick) => {
-//   const isValidUserRegister = useIsValidRegister;
-//   if (isValidUserRegister) {
-//     await createUser(formData);
-//     handelClick();
-//   }
-// };
 
-// const useIsValidRegister = (users, formData) => {
-//   const isValidUser = useIsValidUserName(users, formData);
-//   return (
-//     validPhone(formData?.phone) &&
-//     !isValidUser &&
-//     validPass(formData?.password) &&
-//     validEmail(formData?.email)
-//   );
-// };
 
-// export const useIsValidUserName = (users, formData) => {
-//     const [isValidUser, setIsValidUser] = useState(false);
-
-//       setIsValidUser(
-//         users.map((user) => user.username).includes(formData?.username)
-//       );
-
-//       const res=  (
-//         validPhone(formData?.phone) &&
-//         !isValidUser &&
-//         validPass(formData?.password) &&
-//         validEmail(formData?.email)
-//       )
-//       return {isValidUser,res};
-// };
-
-export const isValidUserName = (formData,isValidUser) => {
-    return (
-      validPhone(formData?.phone) &&
-      !isValidUser &&
-      validPass(formData?.password) &&
-      validEmail(formData?.email)
-    );
-  };
-
+export const isValidUserName = (formData, isValidUser) => {
+  return (
+    validPhone(formData?.phone) &&
+    !isValidUser &&
+    validPass(formData?.password) &&
+    validEmail(formData?.email)
+  );
+};
 
