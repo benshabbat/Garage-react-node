b7ea09731b225feaec99c415503d2b0605ea3a4a
benshabbat <benshabbat27@gmail.com>
2024-11-11 20:33:08 +0200

    external function for register useRegister worked

--- client/src/components/create/utilsCreate.js
+++ client/src/components/create/utilsCreate.js
@@ -1,6 +1,25 @@
 import { useState } from "react";
-import { createReqService } from "../../utils";
+import { createReqService,createUser } from "../../utils";
 import { validPhone, validPass, validEmail } from "../../validation/valid";
+
+
+export function useRegister(users,handelClick){
+  const [formData, setFormData] = useState();
+  const [isValidUser, setIsValidUser] = useState(false);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    setIsValidUser(
+      users.map((user) => user.username).includes(formData?.username)
+    );
+    if (isValidUserName(formData,isValidUser)) {
+      await createUser(formData);
+      handelClick();
+    }
+  };
+  return {setFormData,onSubmit,isValidUser}
+}
+
 export const isValidUserName = (formData, isValidUser) => {
   return (
     validPhone(formData?.phone) &&
@@ -9,6 +28,7 @@ export const isValidUserName = (formData, isValidUser) => {
     validEmail(formData?.email)
   );
 };
+
 
 
 //dosent work
