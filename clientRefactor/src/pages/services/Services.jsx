bbda38ad947d53d7b1a0b03361d43b38bd18cdf8
benshabbat <benshabbat27@gmail.com>
2024-11-11 14:06:40 +0200

    moving services page to refactor

--- clientRefactor/src/pages/services/Services.jsx
+++ clientRefactor/src/pages/services/Services.jsx
@@ -1,33 +1,20 @@
 import "./services.css";
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
 import Service from "../../components/service/Service";
-import { useSelector,useDispatch} from "react-redux"
-import { getServicesByIdCar } from "../../features/user/userSlice";
+import { useServices } from "./utilsServices";
 const Services = () => {
-  const { user } = useSelector((state) => state.user);
-  const { carId } = useParams();
-  const carFilter = user?.cars?.filter((car) => car._id === carId);
-  const { services} = useSelector((state) => state.user);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getServicesByIdCar(carId));
-  }, [user]);
-  //need to work on services for user
+  const { user } = useServices();
   const carService = (car) => {
     return (
       <div key={car?._id}>
         <h2 className="title">{car?.numberPlate}</h2>
-        <Service carServices={services} />
+        <Service />
       </div>
     );
   };
   return (
     <>
       <h1 className="h-title">{`hello ${user?.username}`}</h1>
-      {carId && carFilter
-        ? carFilter?.map(carService)
-        : user?.cars?.map(carService)}
+      {user?.cars?.map(carService)}
     </>
   );
 };
