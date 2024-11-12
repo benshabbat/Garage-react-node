4b9a96b4624ef808238189febf01ca6de7cde260
benshabbat <benshabbat27@gmail.com>
2024-11-12 10:34:36 +0200

    //TODO: TO MAKE CUSTOM COMPONENTS AND TO MOVE THEM TO UTILS ACCOUNT

--- client/src/pages/account/Account.jsx
+++ client/src/pages/account/Account.jsx
@@ -1,23 +1,32 @@
 import "./account.css";
 import ReqService from "../../components/create/ReqService";
-import { useAccount,  } from "./utilsAccount";
+import { useAccount } from "./utilsAccount";
+
+
+//TODO: TO MAKE CUSTOM COMPONENTS AND TO MOVE THEM TO UTILS ACCOUNT
 const Account = () => {
-  const { car, isOpenReqService, handleReqService,useFilterAccount } =
+  const { car, isOpenReqService, handleReqService, useFilterAccount } =
     useAccount();
   const { filterSearch, bodyAccountForTable } = useFilterAccount();
+
+  const FilterSearch = () => {
+    return (
+      <section className="table__header">
+        <h1>My Cars</h1>
+        <div className="input-group">
+          <input
+            type="search"
+            placeholder="Search Data..."
+            onChange={filterSearch}
+          />
+        </div>
+      </section>
+    );
+  };
   return (
     <>
       <div className="table-container">
-        <section className="table__header">
-          <h1>My Cars</h1>
-          <div className="input-group">
-            <input
-              type="search"
-              placeholder="Search Data..."
-              onChange={filterSearch}
-            />
-          </div>
-        </section>
+        <FilterSearch />
         <section className="table__body">
           <table>
             <thead>
