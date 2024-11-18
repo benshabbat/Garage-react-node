02e19f4f88e480d576e83022f090777a21eb2a93
benshabbat <benshabbat27@gmail.com>
2024-11-18 12:23:02 +0200

    AppointmentSchema

---
+++ server/models/Appointment.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const AppointmentSchema = new Schema({
+  clientName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["pending", "confirmed", "cancelled"],
+    default: "pending",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model("Appointment", AppointmentSchema);
