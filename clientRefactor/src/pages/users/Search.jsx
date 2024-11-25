78ac0a71d0044179c88253239fd04c4aa6f17ed2
benshabbat <benshabbat27@gmail.com>
2024-11-25 17:54:26 +0200

    Search user done

--- clientRefactor/src/pages/users/Search.jsx
+++ clientRefactor/src/pages/users/Search.jsx
@@ -1,18 +1,20 @@
+import { useUsersContext } from "./UsersContext";
 
-export default   function Search() {
-    return (
-      <section className="table__header">
-        <h1>Users</h1>
-        <div className="input-group">
-          <input
-            type="search"
-            placeholder="Search Data..."
-            onChange={handleSearch}
-          />
-        </div>
-        <button onClick={handleCreateUser} className="create-button">
-          Create User
-        </button>
-      </section>
-    );
-  }
+export default function Search() {
+  const { handleSearch, handleCreateUser } = useUsersContext();
+  return (
+    <section className="table__header">
+      <h1>Users</h1>
+      <div className="input-group">
+        <input
+          type="search"
+          placeholder="Search Data..."
+          onChange={handleSearch}
+        />
+      </div>
+      <button onClick={handleCreateUser} className="create-button">
+        Create User
+      </button>
+    </section>
+  );
+}
