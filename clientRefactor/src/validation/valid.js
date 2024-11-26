add4cf4cfd0f3e665d2c1e0cd4c26242929061b8
benshabbat <benshabbat27@gmail.com>
2024-11-26 12:26:04 +0200

    ok made input types with title and errormsg for generic into valid and using in forminput

--- clientRefactor/src/validation/valid.js
+++ clientRefactor/src/validation/valid.js
@@ -4,7 +4,34 @@ import validCar from "./validCar.js";
 import validEmail from "./validEmail.js";
 import validPass from "./validPass.js";
 
-
+// Constants for input validation messages and patterns
+const INPUT_TYPES = {
+  email: {
+    title: "regex@gmail.com",
+    errorMessage: "Your Email is wrong",
+  },
+  password: {
+    title:
+      "Must contain at least one number, uppercase and lowercase letter, and at least 8 characters",
+    errorMessage: "Your password is wrong",
+  },
+  phone: {
+    title: "Number of phone must 050-1234567",
+    errorMessage: "Your phone number is wrong",
+  },
+  numberPlate: {
+    title: "Number of car must 00-000-00 OR 000-00-000",
+    errorMessage: "Your Car number is wrong",
+  },
+};
+const inputType = (input) => {
+  return (
+    INPUT_TYPES[input.name] || {
+      title: input.title,
+      errorMessage: input.errorMessage,
+    }
+  );
+};
 
 const valid = (data, type) => {
   switch (type) {
@@ -20,4 +47,13 @@ const valid = (data, type) => {
       return false;
   }
 };
-export { valid, validCar, validPhone, validPass, validEmail,validUser };
+
+export {
+  valid,
+  validCar,
+  validPhone,
+  validPass,
+  validEmail,
+  validUser,
+  inputType,
+};
