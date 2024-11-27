286dd1a5775915acbdbee62efdabe9fd817d7a43
benshabbat <benshabbat27@gmail.com>
2024-11-27 16:27:00 +0200

    //TODO:MODALS WITH CONTEXT

--- clientRefactor/src/pages/servicesAdmin/ServiceAdminModals.jsx
+++ clientRefactor/src/pages/servicesAdmin/ServiceAdminModals.jsx
@@ -2,7 +2,7 @@ import EditStatusService from "../../components/edit/EditStatusService";
 import EditPaidService from "../../components/edit/EditPaidService";
 import ManageService from "../../components/manage/ManageService";
 import { useServicesAdminContext } from "./ServiceAdminContext";
-
+//TODO:MODALS WITH CONTEXT
 export default function ServiceAdminModals() {
     const {selectedService,modals} = useServicesAdminContext()
     return (
