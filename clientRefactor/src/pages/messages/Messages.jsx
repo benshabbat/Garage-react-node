e769b4fa5695ef4e53c87f468da580e9982054a3
benshabbat <benshabbat27@gmail.com>
2024-11-28 12:44:10 +0200

    ok messages wrapped with provider now i stating to use with logic for ui

--- clientRefactor/src/pages/messages/Messages.jsx
+++ clientRefactor/src/pages/messages/Messages.jsx
@@ -1,15 +1,18 @@
+import MessagesModals from "./MessagesModals";
+import MessagesProvider from "./MessagesProvider";
+import MessagesTable from "./MessagesTable";
+import Search from "./Search";
 
 //TODO: to make context for messages
 const Messages = () => {
-
   return (
-    <>
+    <MessagesProvider>
       <div className="table-container">
-        <Search />
-        <TableMessages />
+        <Search/>
+        <MessagesTable/>
       </div>
-
-    </>
+      <MessagesModals/>
+    </MessagesProvider>
   );
 };
 
