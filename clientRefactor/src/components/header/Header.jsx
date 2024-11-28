66ba47e6444942efa6566592fc2c8e21717c9b45
benshabbat <benshabbat27@gmail.com>
2024-11-29 00:41:49 +0200

    deleted the comments the data before context into ui baaa

--- clientRefactor/src/components/header/Header.jsx
+++ clientRefactor/src/components/header/Header.jsx
@@ -4,21 +4,7 @@ import { Suspense } from "react";
 import HeaderProvider from "./HeaderProvider";
 import Navbars from "./Navbars";
 
-// import { useSelector, useDispatch } from "react-redux";
-// import { getUser } from "../../features/user/userSlice";
-//TODO: Context for header with navbars
 const Header = () => {
-  // const [isNavOpen, setIsNavOpen] = useState(false);
-  // const { user: userAuth } = useSelector((state) => state.auth);
-  // const { user } = useSelector((state) => state.user);
-  // const dispatch = useDispatch();
-
-  // useEffect(() => {
-  //   if (userAuth?._id) dispatch(getUser(userAuth?._id));
-  // }, [dispatch, userAuth]);
-
-  // const handleOutsideClick = () => setIsNavOpen(!isNavOpen);
-
   return (
     <>
       <HeaderProvider>
