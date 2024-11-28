99dbef9b280590f6e7ddee5f7aea356dae547a4d
benshabbat <benshabbat27@gmail.com>
2024-11-28 12:56:01 +0200

    ok i changed the handle search function and the data i will need puted into value for using in external of components

--- clientRefactor/src/pages/messages/MessagesProvider.jsx
+++ clientRefactor/src/pages/messages/MessagesProvider.jsx
@@ -7,48 +7,72 @@ import { getUsers } from "../../features/admin/adminSlice";
 import { deleteMessage } from "../../utils";
 import useOpenModel from "../../hooks/useOpenModel";
 
-export default function MessagesProvider({children}) {
-    const { messages, user } = useSelector((state) => state.user);
-    const { users } = useSelector((state) => state.admin);
+export default function MessagesProvider({ children }) {
+  const { messages, user } = useSelector((state) => state.user);
+  const { users } = useSelector((state) => state.admin);
 
-    const [filterMessages, setFilterMessages] = useState();
-    const [isDeleted, setIsDeleted] = useState(false);
+  const [filteredMessages, setFilteredMessages] = useState();
+  const [isDeleted, setIsDeleted] = useState(false);
 
-    const [handleCreateMessage, isOpenCreateMessage] = useOpenModel();
+  const [handleCreateMessage, isOpenCreateMessage] = useOpenModel();
 
-    const dispatch = useDispatch();
+  const displayMessages = filteredMessages || messages;
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    if (user?.isAdmin) dispatch(getUsers());
+    if (user) dispatch(getMessagesByIdUser(user?._id));
+  }, [user, isOpenCreateMessage, isDeleted, dispatch]);
 
-    useEffect(() => {
-        if (user?.isAdmin) dispatch(getUsers());
-        if (user) dispatch(getMessagesByIdUser(user?._id));
-      }, [user, isOpenCreateMessage, isDeleted]);
-    
-      const filterSearch = (e) => {
-        setFilterMessages(
-          messages?.filter(item => 
-            Object.values({
-              username: item?.from?.username,
-              toUsername: item?.to?.username,
-              title: item?.title,
-              description: item?.description,
-              date: item?.updatedAt
-            }).some(value => value?.toLowerCase().includes(e.target.value.toLowerCase()))
+  //   const filterSearch = (e) => {
+  //     setFilterMessages(
+  //       messages?.filter((item) =>
+  //         Object.values({
+  //           username: item?.from?.username,
+  //           toUsername: item?.to?.username,
+  //           title: item?.title,
+  //           description: item?.description,
+  //           date: item?.updatedAt,
+  //         }).some((value) =>
+  //           value?.toLowerCase().includes(e.target.value.toLowerCase())
+  //         )
+  //       )
+  //     );
+  //   };
+  const handleSearch = (e) => {
+    const { value } = e.target;
+    setFilteredMessages(
+      value
+        ? messages.filter(
+            (item) =>
+              item?.from?.username.includes(value) ||
+              item?.to?.username.includes(value) ||
+              item?.title.includes(value) ||
+              item?.description.includes(value) ||
+              item?.updatedAt.includes(value)
           )
-        );
-      };
+        : null
+    );
+  };
 
+  const handleDelete = async (messageId) => {
+    await deleteMessage(messageId);
+    setIsDeleted((prev) => !prev);
+  };
 
-      const handleDelete = async (messageId) => {
-        await deleteMessage(messageId);
-        setIsDeleted(prev => !prev);
-      };
+  const value = {
+      users,
+    displayMessages,
+    handleDelete,
+    handleSearch,
+    modlas: {
+      createMsg: { isOpen: isOpenCreateMessage, onClose: handleCreateMessage },
+    },
+  };
 
-const value={
-
-}
-
-
-  return <MessagesContext.Provider value={value}>{children}</MessagesContext.Provider>
-
+  return (
+    <MessagesContext.Provider value={value}>
+      {children}
+    </MessagesContext.Provider>
+  );
 }
