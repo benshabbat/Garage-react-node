569f1bcdc62438063326764cabd91fad5f7c9840
benshabbat <benshabbat27@gmail.com>
2024-11-29 15:23:38 +0200

    //TODO: i dont need this props i can using with context

--- clientRefactor/src/pages/cars/CarModals.jsx
+++ clientRefactor/src/pages/cars/CarModals.jsx
@@ -3,7 +3,7 @@ import EditCar from "../../components/edit/EditCar";
 import CreateService from "../../components/create/CreateService";
 import DeleteCar from "../../components/delete/DeleteCar";
 import { useCarsContext } from "./CarsContext";
-
+//TODO: i dont need this props i can using with context
 export default function CarModals() {
   const { modals } = useCarsContext();
   return (
