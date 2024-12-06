3c929899b19cb5bd962ecdea8a0d750968639cfa
benshabbat <benshabbat27@gmail.com>
2024-12-06 09:59:04 +0200

    now css for new contact form

--- clientRefactor/src/components/landing/contact/newContact.jsx
+++ clientRefactor/src/components/landing/contact/newContact.jsx
@@ -2,10 +2,14 @@ export default function newContact() {
   return (
     <div className="container">
       <form>
+        <h1>Contact Us</h1>
         <input type="text" id="firstName" placeholder="first name" required />
         <input type="text" id="lasstName" placeholder="last name" required />
         <input type="text" id="email" placeholder="email" required />
         <input type="text" id="phone" placeholder="phone" required />
+        <h4>Type your message here...</h4>
+        <textarea required></textarea>
+        <button></button>
       </form>
     </div>
   );
