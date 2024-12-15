81407fb4a853ed17865bcf7b794e06d1c72b5255
benshabbat <benshabbat27@gmail.com>
2024-12-15 09:55:34 +0200

    deleted old client

--- oldClient/src/components/landing/about/About.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const About = () => {
-  return (
-    <div id='about' style={{height:500}}>About</div>
-  )
-}
-
-export default About
--- oldClient/src/pages/Users.jsx
+++
@@ -1,9 +0,0 @@
-import { useUsers } from "./utilsUsers";
-
-const Users = () => {
-  const { PageUsers } = useUsers();
-
-  return PageUsers();
-};
-
-export default Users;
