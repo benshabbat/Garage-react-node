df6077e9597cc713461893e5012978872d6ba5f6
benshabbat <benshabbat27@gmail.com>
2024-12-15 16:32:06 +0200

    deleted what i dont need anymore

--- client/src/pages/pageLanding/PageLanding.jsx
+++ client/src/pages/pageLanding/PageLanding.jsx
@@ -1,15 +1,11 @@
 import "./pageLanding.css";
-import Home from "../home/Home";
-import { Reviews, About, ServicesLanding, Contact } from "../../components";
+import { Reviews, About} from "../../components";
 import NewContact from "../../components/landing/contact/newContact";
 
 const PageLanding = () => {
   return (
     <div className="main">
-      {/* <Home /> */}
       <About />
-      {/* <ServicesLanding /> */}
-      {/* <Contact /> */}
       <NewContact/>
       <Reviews />
       <footer>Written By David-Chen Benshabbat</footer>
