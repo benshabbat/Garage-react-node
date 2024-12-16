95463b7b67378d87812a9f08847105360c1c17ea
benshabbat <benshabbat27@gmail.com>
2024-12-16 23:31:02 +0200

    change cors to the new client address

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cors());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://garage-client-one.vercel.app',
   credentials: true
 }));
 
