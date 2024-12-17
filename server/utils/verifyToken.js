c309b630c5fff8d21d4a3e6a74c75573fa9cb87d
benshabbat <benshabbat27@gmail.com>
2024-12-17 13:23:57 +0200

    added conditions

--- server/utils/verifyToken.js
+++ server/utils/verifyToken.js
@@ -1,36 +1,91 @@
+// import jwt from "jsonwebtoken";
+// import { createError } from "../utils/error.js";
+
+// export const verifyToken = (req, res, next) => {
+//   const token = req.cookies.access_token;
+//   if (!token) return next(createError(401, "Your are not authenticated!"));
+//   jwt.verify(token, process.env.JWT, (err, user) => {
+//     if (err) {
+//       return next(createError(403, "Token is not valid!"));
+//     } else {
+//       req.user = user;
+//       next();
+//     }
+//   });
+// };
+
+// export const verifyUser = (req, res, next) => {
+//   verifyToken(req, res, () => {
+//     if (req.user.id === req.params.id || req.user.isAdmin||req.user.id) {
+//       return next();
+//     } else {
+//       return next(createError(403, "You are not auth"));
+//     }
+//   });
+// };
+
+
+// export const verifyAdmin = (req, res, next) => {
+//   verifyToken(req, res, () => {
+//     if (req.user.isAdmin) {
+//       return next();
+//     } else {
+//       return next(createError(403, "You are not auth"));
+//     }
+//   });
+// };
+
+
 import jwt from "jsonwebtoken";
 import { createError } from "../utils/error.js";
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
-  if (!token) return next(createError(401, "Your are not authenticated!"));
+  if (!token) {
+    return next(createError(401, "You are not authenticated!"));
+  }
+
   jwt.verify(token, process.env.JWT, (err, user) => {
     if (err) {
       return next(createError(403, "Token is not valid!"));
-    } else {
-      req.user = user;
-      next();
     }
+    req.user = user;
+    next();
   });
 };
 
 export const verifyUser = (req, res, next) => {
-  verifyToken(req, res, () => {
-    if (req.user.id === req.params.id || req.user.isAdmin||req.user.id) {
-      return next();
+  verifyToken(req, res, (err) => {
+    if (err) {
+      return next(err);
+    }
+    
+    if (!req.user) {
+      return next(createError(403, "User not authenticated"));
+    }
+
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
     } else {
-      return next(createError(403, "You are not auth"));
+      next(createError(403, "You are not authorized"));
     }
   });
 };
 
-
 export const verifyAdmin = (req, res, next) => {
-  verifyToken(req, res, () => {
+  verifyToken(req, res, (err) => {
+    if (err) {
+      return next(err);
+    }
+    
+    if (!req.user) {
+      return next(createError(403, "User not authenticated"));
+    }
+
     if (req.user.isAdmin) {
-      return next();
+      next();
     } else {
-      return next(createError(403, "You are not auth"));
+      next(createError(403, "You are not authorized as admin"));
     }
   });
 };
