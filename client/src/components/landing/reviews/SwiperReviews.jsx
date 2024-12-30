5cfed0ca3f57f4bb99951b7b129316a5fecf20fe
benshabbat <benshabbat27@gmail.com>
2024-12-30 08:02:01 +0200

    ok one more try added if not

--- client/src/components/landing/reviews/SwiperReviews.jsx
+++ client/src/components/landing/reviews/SwiperReviews.jsx
@@ -3,33 +3,33 @@ import { useReviewsContext } from "./ReviewsContext";
 import Swiper from "./swiper/Swiper";
 
 const CheckCircle = () => (
-  <svg 
-    className="success-icon" 
-    viewBox="0 0 24 24" 
-    fill="none" 
+  <svg
+    className="success-icon"
+    viewBox="0 0 24 24"
+    fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <circle 
-      cx="12" 
-      cy="12" 
-      r="10" 
-      className="check-circle" 
-      stroke="currentColor" 
+    <circle
+      cx="12"
+      cy="12"
+      r="10"
+      className="check-circle"
+      stroke="currentColor"
       strokeWidth="2"
     />
-    <path 
-      d="M8 12.5L10.5 15L16 9" 
-      stroke="currentColor" 
-      strokeWidth="2" 
-      strokeLinecap="round" 
+    <path
+      d="M8 12.5L10.5 15L16 9"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
       strokeLinejoin="round"
       className="check-mark"
     />
   </svg>
 );
 export default function SwiperReviews() {
-  const { allReviews,useAddReview } = useReviewsContext();
-  const {isSubmitted,setIsSubmitted } = useAddReview();
+  const { allReviews, useAddReview } = useReviewsContext();
+  const { isSubmitted, setIsSubmitted } = useAddReview();
 
   if (isSubmitted) {
     return (
@@ -38,7 +38,7 @@ export default function SwiperReviews() {
           <CheckCircle className="success-icon" />
           <h2 className="success-title">Thank You!</h2>
           <p>Your message has been sent successfully.</p>
-          <button 
+          <button
             onClick={() => setIsSubmitted(false)}
             className="new-message-button"
           >
@@ -48,11 +48,13 @@ export default function SwiperReviews() {
       </div>
     );
   }
-  return (
-    <Swiper>
-      {allReviews?.map((customer, index) => (
-        <Review customer={customer} key={index} />
-      ))}
-    </Swiper>
-  );
+  if (!isSubmitted) {
+    return (
+      <Swiper>
+        {allReviews?.map((customer, index) => (
+          <Review customer={customer} key={index} />
+        ))}
+      </Swiper>
+    );
+  }
 }
