3ff419e3a31be8b230e153042980c4fd560326e8
benshabbat <benshabbat27@gmail.com>
2025-01-21 13:24:03 +0200

    try back css

--- client/src/components/landing/reviews/swiper/Swiper.jsx
+++ client/src/components/landing/reviews/swiper/Swiper.jsx
@@ -3,21 +3,21 @@ import NextCard from "./NextCard";
 import Pagination from "./Pagination";
 import PrevCard from "./Prevcard";
 import Slides from "./Slides";
-
 const Swiper = ({ children }) => {
+  
   return (
-    <div className="swiper-outer-container">
-      <div className="swiper-container">
-        <div className="swiper-wrapper">
-          <PrevCard />
-          <div className="swiper-content">
-            <Slides>{children}</Slides>
+      <div className="swiper-outer-container">
+        <div className="swiper-container">
+          <div className="swiper-wrapper">
+            <PrevCard />
+            <div className="swiper-content">
+              <Slides>{children}</Slides>
+            </div>
+            <NextCard />
           </div>
-          <NextCard />
+          <Pagination />
         </div>
-        <Pagination />
       </div>
-    </div>
   );
 };
 
