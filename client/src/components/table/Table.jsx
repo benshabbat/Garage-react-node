86629fd339d287561dd90d5232f3466b9fe49c99
benshabbat <benshabbat27@gmail.com>
2025-01-26 15:23:15 +0200

    TODO

--- client/src/components/table/Table.jsx
+++ client/src/components/table/Table.jsx
@@ -1,7 +1,7 @@
 import { Children, cloneElement, useState } from "react";
 
 
-//need to understand this code about sorting
+//TODO:need to understand this code about sorting
 const Table = ({ trTh, trTd }) => {
   const [sortConfig, setSortConfig] = useState({
     key: null,
