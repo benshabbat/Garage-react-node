f7cca05177820fa4e114ae85727457d92ab8e60b
benshabbat <benshabbat27@gmail.com>
2025-01-26 15:13:05 +0200

    fixvalidphone

--- client/src/validation/validPhone.js
+++ client/src/validation/validPhone.js
@@ -1,8 +1,9 @@
-const PHONE_REGEX = /^[0-9]{3}[-][0-9]{7}|[0-9]{10}|[0-9]{3}-[0-9]{3}-[0-9]{4}$/;
+const PHONE_REGEX = /^[0-9]{3}-[0-9]{3}-[0-9]{4}|[0-9]{3}[-][0-9]{7}|[0-9]{10}$/;
 const validPhone = (data) => {
     if (
       (data?.length === 10 && +data) ||
-      (data?.length === 11 && data.at(3) === "-")
+      (data?.length === 11 && data.at(3) === "-")||
+      (data?.length === 12 && data.at(3) === "-" && data.at(7) === "-")
     ) {
       return PHONE_REGEX.test(data);
     } else return false;
