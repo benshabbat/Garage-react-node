7ae04214cfa6ce90f14d3941ea007696d035ae92
benshabbat <benshabbat27@gmail.com>
2025-01-28 20:39:34 +0200

    useContextMessages after i will work on rename

--- client/src/pages/messages/MessagesConetxt.jsx
+++ client/src/pages/messages/MessagesConetxt.jsx
@@ -2,5 +2,5 @@ import { createContext } from "react";
 import { createContextHook } from "../../components/context/useContextGeneric";
 
 export const MessagesContext = createContext(null);
-export const useMessagesContext = createContextHook(MessagesContext, "useMessagesContext");
+export const useContextMessages = createContextHook(MessagesContext, "useContextMessages");
 
