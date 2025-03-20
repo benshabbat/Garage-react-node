e4bf58006b2a8de75917e66055b57a994ab1350e
benshabbat <benshabbat27@gmail.com>
2025-03-20 11:16:45 +0200

    refactor: update API URL constants in userService.js and utils.js for consistency

--- client/src/features/user/userService.js
+++ client/src/features/user/userService.js
@@ -1,5 +1,10 @@
 import axios from "../../axiosConfig.js";
-import { ADMIN, API_URL_MESSAGES, API_URL_SERVICE, API_URL_USER } from "../../utils.js";
+
+const API_URL_USER = "/users";
+const API_URL_CAR = "/cars";
+const API_URL_SERVICES = "/services";
+const API_URL_MESSAGES = "/messages";
+const ADMIN = "63e14deca4340e45d23f20b2";
 
 const createReqService = async (dataMessage) => {
   const { data } = await axios.post(
@@ -18,12 +23,12 @@ const getUser = async (id) => {
 };
 
 const getServicesByIdCar = async (carId) => {
-  const { data } = await axios.get(`${API_URL_SERVICE}/car/${carId}`);
+  const { data } = await axios.get(`${API_URL_SERVICES}/car/${carId}`);
 
   return data;
 };
 const getServicesByIdUser = async (userId) => {
-  const { data } = await axios.get(`${API_URL_SERVICE}/user/${userId}`);
+  const { data } = await axios.get(`${API_URL_SERVICES}/user/${userId}`);
 
   return data;
 };
