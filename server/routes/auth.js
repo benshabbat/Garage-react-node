08fe0f7a1630bf3858b7041733905f883451cf21
benshabbat <benshabbat27@gmail.com>
2025-03-20 12:59:23 +0200

    refactor: add verifyAdmin middleware to register route for enhanced security

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,7 +3,7 @@ import { register,login,logout } from "../controllers/auth.js";
 import { verifyAdmin} from "../utils/verifyToken.js";
 const router = express.Router();
 
-router.post("/register",register );
+router.post("/register",verifyAdmin,register );
 router.post("/login",login );
 router.post("/logout", logout)
 export default router
