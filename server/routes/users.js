f6632538a06960700983583e9b77a054ed685fe8
benshabbat <benshabbat27@gmail.com>
2025-03-22 21:42:37 +0200

    refactor: improve token verification logic and clean up user route handlers

--- server/routes/users.js
+++ server/routes/users.js
@@ -6,30 +6,19 @@ import {
   getUsers,
   getUsersByType,
 } from "../controllers/user.js";
-import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
+import { verifyAdmin, verifyUser } from "../utils/verifyToken.js";
 const router = express.Router();
-/*
-router.get("/checkauthentication",verifyToken,(req,res,next)=>{
-    res.send("logged in")
-})
-router.get("/checkuser/:id",verifyUser,(req,res,next)=>{
-    res.send("hello user logged in :)")
-})
-router.get("/checkadmin/:id",verifyAdmin,(req,res,next)=>{
-    res.send("hello admin logged in :)")
-})
-*/
-//TEST CREATE USER
+
 //GET ALL BY POPULATE
 router.get("/populate", verifyAdmin, getUsersByType);
+//GET ALL
+router.get("/", verifyAdmin, getUsers);
 
+//GET
+router.get("/:id", verifyUser, getUser);
 //UPDATE
 router.put("/:id", verifyAdmin, updateUser);
 //DELETE
 router.delete("/:id", verifyAdmin, deleteUser);
-//GET
-router.get("/:id", verifyUser, getUser);
-//GET ALL
-router.get("/", verifyAdmin, getUsers);
 
 export default router;
--- server/utils/verifyToken.js
+++ server/utils/verifyToken.js
@@ -2,58 +2,46 @@ import jwt from "jsonwebtoken";
 import { createError } from "../utils/error.js";
 
 export const verifyToken = (req, res, next) => {
-  console.log("Cookies:", req.cookies);
-  const token = req.cookies.access_token;
-  if (!token) {
-    console.log("No token found");
-    return next(createError(401, "Not authenticated"));
-  }
+  try {
+    const token = req.cookies.access_token;
+    
+    if (!token) {
+      return next(createError(401, "Not authenticated"));
+    }
 
-  jwt.verify(token, process.env.JWT, (err, user) => {
-    if (err) console.log("Token verification failed:", err);
-    req.user = user;
-    next();
-  });
+    jwt.verify(token, process.env.JWT, (err, user) => {
+      if (err) {
+        return next(createError(403, "Token is not valid"));
+      }
+      
+      req.user = user;
+      next();
+    });
+  } catch (err) {
+    next(createError(500, "Error verifying token"));
+  }
 };
 
 export const verifyUser = (req, res, next) => {
   verifyToken(req, res, (err) => {
-    if (err) {
-      console.log("Token verification failed:", err);
-      return next(err);
-    }
-
-    console.log("User in request:", req.user);
-    console.log("Params ID:", req.params.id);
-
-    if (!req.user || !req.user.id) {
-      return next(createError(403, "Authentication failed - no user data"));
-    }
-
+    if (err) return next(err);
+    
     if (req.user.id === req.params.id || req.user.isAdmin) {
       return next();
     }
-
-    return next(
-      createError(403, "You are not authorized to access this resource")
-    );
+    
+    return next(createError(403, "You are not authorized to access this resource"));
   });
 };
 
 export const verifyAdmin = (req, res, next) => {
   verifyToken(req, res, (err) => {
-    if (err) {
-      return next(err);
+    if (err) return next(err);
+    
+    if (req.user.isAdmin) {
+      return next();
     }
-
-    if (!req.user) {
-      return next(createError(403, "User not authenticated"));
-    }
-
-    if (req?.user?.isAdmin) {
-      next();
-    } else {
-      next(createError(403, "You are not authorized as admin"));
-    }
+    
+    return next(createError(403, "You are not authorized as admin"));
   });
 };
