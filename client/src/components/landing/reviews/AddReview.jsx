47aad472c3df070974551aa8ac4262d632ebc27a
benshabbat <benshabbat27@gmail.com>
2025-03-24 21:28:08 +0200

    refactor: enhance button styling and layout in AddReview component

--- client/src/components/landing/reviews/AddReview.jsx
+++ client/src/components/landing/reviews/AddReview.jsx
@@ -4,11 +4,11 @@ import { useReviewsContext } from "./ReviewsContext";
 export default function AddReview() {
   const {handleAddReview} =useReviewsContext()
   return (
-    <>
+    <div className="button-container">
       <button className="btn-add-review" onClick={handleAddReview}>
         Share Your Experience
       </button>
    
-    </>
+    </div>
   );
 }
