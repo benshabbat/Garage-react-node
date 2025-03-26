9133a24d5e80fdb18eb7704293dc9198c8ab0e28
benshabbat <benshabbat27@gmail.com>
2025-03-27 01:05:13 +0200

    fix: update filtered services mapping to use updated service data

--- client/src/pages/servicesAdmin/ServiceAdminProvider.jsx
+++ client/src/pages/servicesAdmin/ServiceAdminProvider.jsx
@@ -83,7 +83,7 @@ export default function ServiceAdminProvider({ children }) {
       handleClick();
       setFilteredServices(
         services.map((service) =>
-          (service._id === selectedService?._id ? updated : service
+          (service._id === selectedService?._id ? updated.data : service
         ))
       );
     };
