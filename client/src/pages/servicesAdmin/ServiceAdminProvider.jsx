c20244b28f47c84307a8be07388dc78da285e011
benshabbat <benshabbat27@gmail.com>
2025-03-27 01:31:40 +0200

    fix: simplify filtered services mapping to directly use updated service data

--- client/src/pages/servicesAdmin/ServiceAdminProvider.jsx
+++ client/src/pages/servicesAdmin/ServiceAdminProvider.jsx
@@ -83,9 +83,10 @@ export default function ServiceAdminProvider({ children }) {
       handleClick();
       setFilteredServices(
         services.map((service) =>
-          service._id === selectedService?._id ? {[service.car.numberPlate]:service.car.numberPlate, ...updated.data} : service
-        )
-      );
+          service._id === selectedService?._id ?  updated.data : service
+      )
+    );
+    console.log(services)
     };
     return { onSubmit, formData, setFormData };
   };
