10c27eed0455933a188c91f789e3a0ef4766d6a8
benshabbat <benshabbat27@gmail.com>
2025-03-26 23:48:02 +0200

    refactor: improve readability of FormInput component by restructuring code and enhancing error handling logic

--- client/src/components/form/FormInput.jsx
+++ client/src/components/form/FormInput.jsx
@@ -3,23 +3,30 @@ import { valid, inputType } from "../../validation/valid";
 import FormToggle from "./FormToggle";
 
 //ClassNameLabel is when i using with another stlyle for form and inputs
-export default function FormInput({ input, handleChange, isFocus, index,classNameLabel }) {
+export default function FormInput({
+  input,
+  handleChange,
+  isFocus,
+  index,
+  classNameLabel,
+}) {
   const [isBlur, setIsBlur] = useState(false);
   const inputRef = useRef();
 
-  const showError = isBlur && (
-    input.isError || 
-    (input.name !== "username" && !valid(inputRef?.current?.value, input.name))
-  );
+  const showError =
+    !input?.value &&
+    isBlur &&
+    (input.isError ||
+      (input.name !== "username" &&
+        !valid(inputRef?.current?.value, input.name)));
 
   if (input.type === "checkbox") {
     return <FormToggle input={input} handleChange={handleChange} />;
   }
 
-  
   return (
-    <label className={classNameLabel??"form-label"}>
-      {!input.hidden &&!classNameLabel &&<span>{input.name}</span>}
+    <label className={classNameLabel ?? "form-label"}>
+      {!input.hidden && !classNameLabel && <span>{input.name}</span>}
       {showError && (
         <span className="error">{inputType(input).errorMessage}</span>
       )}
