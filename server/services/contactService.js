6d33512589567fb28c3741bf92f3279712f46774
benshabbat <benshabbat27@gmail.com>
2025-03-30 12:37:54 +0300

    fix: refactor contact service methods for improved error handling and code clarity

--- server/services/contactService.js
+++ server/services/contactService.js
@@ -2,46 +2,42 @@ import Contact from "../models/Contact.js";
 import { templatePhone } from "../utils/templates.js";
 
 const createContact = async (req) => {
-    const newContact = new Contact(req.body);
-    const {phone} = req.body;
+  try {
+    const { phone } = req.body;
     const newPhone = templatePhone(phone);
-    try {
-      const savedContact = await newContact.save(
-        {
-          ...req.body,
-          phone: newPhone,
-        }
-      );
-      return savedContact;
-    } catch (error) {
-      throw Error(error);
-    }
-  };
+    newContact = new Contact({
+      ...req.body,
+      phone: newPhone,
+    });
+    const savedContact = await newContact.save();
+    return savedContact;
+  } catch (error) {
+    throw Error(error);
+  }
+};
 
-  const getContacts = async () => {
-    try {
-      const contacts = await Contact.find();
-      return contacts;
-    } catch (error) {
-      throw Error(error);
-    }
-  };
+const getContacts = async () => {
+  try {
+    const contacts = await Contact.find();
+    return contacts;
+  } catch (error) {
+    throw Error(error);
+  }
+};
 
-  const deleteContact = async (req) => {
-    try {
-      await Contact.findByIdAndDelete(req.params.id);
-      return "The Message has been removed";
-    } catch (error) {
-      throw Error(error);
-    }
-  };
+const deleteContact = async (req) => {
+  try {
+    await Contact.findByIdAndDelete(req.params.id);
+    return "The Message has been removed";
+  } catch (error) {
+    throw Error(error);
+  }
+};
 
-  const contactService = {
-    createContact,
-    getContacts,
-    deleteContact
-   
-  };
-  
-  export default contactService;
-  
+const contactService = {
+  createContact,
+  getContacts,
+  deleteContact,
+};
+
+export default contactService;
