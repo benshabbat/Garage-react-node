b13ad3598e4e41fff39fbd3a54c8964968087a87
benshabbat <benshabbat27@gmail.com>
2025-04-01 15:44:12 +0300

    fix: improve error message handling in FormInput component by restoring necessary logic and enhancing accessibility attributes

--- client/src/components/form/FormInput.jsx
+++ client/src/components/form/FormInput.jsx
@@ -14,30 +14,29 @@ export default function FormInput({
   const [isBlur, setIsBlur] = useState(false);
   const inputRef = useRef();
 
-  // const showError =
-  //   isBlur &&
-  //   ((input.isError && !input.isExist) ||
-  //     (input.isExist && !input.isError) ||
-  //     !valid(inputRef?.current?.value, input.name));
-  // let errorMessage = null;
-  // // if (showError&& !inputRef?.current?.value) {
-  // //     errorMessage = "This field is required";
-  // // } else 
-  // if (showError) {
-  //   errorMessage = inputType(input).errorMessage;
-  // }
+  const showError =
+    isBlur &&
+    ((input.isError && !input.isExist) ||
+      (input.isExist && !input.isError) ||
+      !valid(inputRef?.current?.value, input.name));
+  let errorMessage = null;
+  if (showError&& !inputRef?.current?.value) {
+      errorMessage = "This field is required";
+  } else if (showError) {
+    errorMessage = inputType(input).errorMessage;
+  }
   const handleBlur = useCallback(() => setIsBlur(true), []);
   if (input.type === "checkbox") {
     return <FormToggle input={input} handleChange={handleChange} />;
   }
-  // if (input.isExist) errorMessage = input.errorExist;
+  if (input.isExist) errorMessage = input.errorExist;
 
   return (
     <label className={classNameLabel}>
       {!input.hidden && !classNameLabel && <span>{input.name}</span>}
-      {input.errorMessage && (
+      {errorMessage && (
         <span id={`${input.name}-error`} className="error">
-          {input.errorMessage}
+          {errorMessage}
         </span>
       )}
       <input
@@ -53,8 +52,8 @@ export default function FormInput({
         title={inputType(input).title}
         onChange={handleChange}
         onBlur={handleBlur}
-        // aria-invalid={showError}
-        // aria-describedby={errorMessage ? `${input.name}-error` : undefined}
+        aria-invalid={showError}
+        aria-describedby={errorMessage ? `${input.name}-error` : undefined}
         required={input.type !== "checkbox"}
         autoComplete="off"
       />
