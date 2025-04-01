1a9f39d0b74d13da4151c02c89c0372dc26ef4cb
benshabbat <benshabbat27@gmail.com>
2025-04-01 15:27:28 +0300

    fix: simplify error message handling in FormInput component by removing unused logic

--- client/src/components/form/FormInput.jsx
+++ client/src/components/form/FormInput.jsx
@@ -14,30 +14,30 @@ export default function FormInput({
   const [isBlur, setIsBlur] = useState(false);
   const inputRef = useRef();
 
-  const showError =
-    isBlur &&
-    ((input.isError && !input.isExist) ||
-      (input.isExist && !input.isError) ||
-      !valid(inputRef?.current?.value, input.name));
-  let errorMessage = null;
-  // if (showError&& !inputRef?.current?.value) {
-  //     errorMessage = "This field is required";
-  // } else 
-  if (showError) {
-    errorMessage = inputType(input).errorMessage;
-  }
+  // const showError =
+  //   isBlur &&
+  //   ((input.isError && !input.isExist) ||
+  //     (input.isExist && !input.isError) ||
+  //     !valid(inputRef?.current?.value, input.name));
+  // let errorMessage = null;
+  // // if (showError&& !inputRef?.current?.value) {
+  // //     errorMessage = "This field is required";
+  // // } else 
+  // if (showError) {
+  //   errorMessage = inputType(input).errorMessage;
+  // }
   const handleBlur = useCallback(() => setIsBlur(true), []);
   if (input.type === "checkbox") {
     return <FormToggle input={input} handleChange={handleChange} />;
   }
-  if (input.isExist) errorMessage = input.errorExist;
+  // if (input.isExist) errorMessage = input.errorExist;
 
   return (
     <label className={classNameLabel}>
       {!input.hidden && !classNameLabel && <span>{input.name}</span>}
-      {errorMessage && (
+      {input.errorMessage && (
         <span id={`${input.name}-error`} className="error">
-          {errorMessage}
+          {input.errorMessage}
         </span>
       )}
       <input
