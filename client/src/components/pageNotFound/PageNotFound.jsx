6e7be929157c84a4cb5ae626d8f666f55f804afb
benshabbat <benshabbat27@gmail.com>
2025-04-11 01:19:15 +0300

    fix: clean up PageNotFound component formatting and add missing newline

--- client/src/components/pageNotFound/PageNotFound.jsx
+++ client/src/components/pageNotFound/PageNotFound.jsx
@@ -1,9 +1,7 @@
-
-//TODO : change import for bg404
 const PageNotFound = () => {
   return (
-    <img src={require("../../images/garage404.jpg")} alt='404' />
-  )
-}
+    <img src={require("../../images/garage404.jpg")} alt="404" />
+  );
+};
 
-export default PageNotFound
+export default PageNotFound;
