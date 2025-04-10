288aacb991d0eff6c8341b8434dcc49ce095f123
benshabbat <benshabbat27@gmail.com>
2025-04-11 01:23:35 +0300

    feat: add 404 message to PageNotFound component

--- client/src/components/pageNotFound/PageNotFound.jsx
+++ client/src/components/pageNotFound/PageNotFound.jsx
@@ -1,7 +1,11 @@
 import "./pageNotFound.css";
 
 const PageNotFound = () => {
-  return <div className="not-found"></div>;
+  return (
+    <div className="not-found">
+      <h1>404 - הדף לא נמצא</h1>
+    </div>
+  );
 };
 
 export default PageNotFound;
