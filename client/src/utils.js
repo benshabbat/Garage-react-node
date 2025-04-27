16f4682264cb4cb98ea5e427b623638a7f11c198
benshabbat <benshabbat27@gmail.com>
2025-04-27 15:41:10 +0300

    style: format getAll function and update export statement for getUsersLocal

--- client/src/utils.js
+++ client/src/utils.js
@@ -8,7 +8,7 @@ export const API_URL_REVIEWS = "/reviews";
 export const API_URL_CONTACTS = "/contacts";
 export const ADMIN = "63e14deca4340e45d23f20b2";
 const getAll = (url) => {
- return axios.get(url);
+  return axios.get(url);
 };
 const getUsersLocal = () => {
   return axios.get(API_URL_USER);
@@ -100,7 +100,7 @@ export {
   getContacts,
   createContact,
   deleteContact,
-  getUsersLocal
+  getUsersLocal,
 };
 
 export function getMomentFromUpdatedAt(updatedAt) {
