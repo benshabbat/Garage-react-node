cca85a127ab605ccb4d784c75ef29872662b1062
benshabbat <benshabbat27@gmail.com>
2025-07-07 17:53:47 +0300

    refactor: enhance table layout for better responsiveness and improve CarsContext code formatting

--- client/src/pages/cars/CarsContext.jsx
+++ client/src/pages/cars/CarsContext.jsx
@@ -1,14 +1,8 @@
-import { createContext} from "react";
+import { createContext } from "react";
 import { createContextHook } from "../../components/context/useContextGeneric";
 
-
-
-
 export const CarsContext = createContext(null);
-
 
 //Provider then
 //useCarsContext have the values of logic
 export const useCarsContext = createContextHook(CarsContext, "useCarsContext");
-
-
