ce22a3c819e202c6811107edf1efe21afcde3147
benshabbat <benshabbat27@gmail.com>
2026-02-24 12:55:53 +0200

    refactor: remove TODO comment in Reviews component for clarity

--- client/src/components/landing/reviews/Reviews.jsx
+++ client/src/components/landing/reviews/Reviews.jsx
@@ -3,7 +3,6 @@ import ReviewsProvider from "./ReviewsProvider";
 import { CreateReviews } from "../../index";
 import ViewReviews from "./ViewReviews";
 
-//TODO:CONTEXT AND PROVIDER
 const Reviews = () => {
   return (
     <ReviewsProvider>
