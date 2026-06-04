592bad6e89caf763d7c2124c59090b030c325562
benshabbat <benshabbat27@gmail.com>
2026-06-04 08:22:28 +0300

    fix: make MyAccount dropdown keyboard accessible with aria-expanded and toggle state (closes #175)

--- client/src/components/myAccount/MyAccount.jsx
+++ client/src/components/myAccount/MyAccount.jsx
@@ -1,4 +1,5 @@
 import "./myAccount.css";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import { useUserStore } from "../../stores/userStore";
@@ -7,15 +8,29 @@ import { useHeaderHandlers } from "../header/hooks/useHeaderHandlers";
 const MyAccount = () => {
   const user = useUserStore((s) => s.user);
   const { handleLogout, handleOutsideClick } = useHeaderHandlers();
+  const [open, setOpen] = useState(false);
+
+  const close = () => setOpen(false);
 
   return (
-    <div className="dropdown">
-      <button>My Account</button>
+    <div
+      className={`dropdown${open ? " is-open" : ""}`}
+      onBlur={(e) => {
+        if (!e.currentTarget.contains(e.relatedTarget)) close();
+      }}
+    >
+      <button
+        aria-haspopup="true"
+        aria-expanded={open}
+        onClick={() => setOpen((o) => !o)}
+      >
+        {user?.username || "My Account"}
+      </button>
       <div className="dropdown-content">
-        <Link to={`/myCars`} onClick={handleOutsideClick}>
+        <Link to="/myCars" onClick={() => { handleOutsideClick(); close(); }}>
           {user?.username}
         </Link>
-        <button onClick={handleLogout}>LogOut</button>
+        <button onClick={() => { handleLogout(); close(); }}>LogOut</button>
       </div>
     </div>
   );
